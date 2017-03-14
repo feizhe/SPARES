742ea231b5877fc0ae0dadc5c2b637d6d369bff7
feizhe <feizhe@users.noreply.github.com>
2017-03-14 11:49:14 -0400

    Add files via upload

--- SPARES example.R
+++ SPARES example.R
@@ -5,41 +5,44 @@ n=150
 p=500
 p0=5
 
+#########beta^0 and the active set
 s0<-sort(sample(1:p,p0))
 beta0<-rep(0,p)
-beta0[s0]<-runif(p0)+0.5
-cbind(s0,beta0[s0])
+beta0[s0]<-runif(p0,0.5,2)
+round(cbind(s0,beta0[s0]),2)
 
-################AR(1)
+################AR(1) correlation structure
 rho=0.8
 H <- abs(outer(1:p, 1:p, "-")) 
 sigma <- rho^H 
 dim(sigma)
-sigma[1:10,1:10]
+round(sigma[1:10,1:10],2)
 
+######generate data
 xmat<-mvrnorm(n=n,mu=rep(0,p),Sigma=sigma)
 err<-rnorm(n,1,1)
 yvec<-xmat%*%beta0+err
 
-l0<-cv.glmnet(xmat,yvec)
-lam0<-l0$lambda.1se
-coef0<-coef(l0,s=lam0)[-1]
-set0<-(1:p)[coef0!=0]
-set0
+##########oracle estimates
+lm0<-lm(yvec~xmat[,s0])
+summary(lm0)
 
-sp1<-SPARE(scale(xmat),yvec-mean(yvec),selection="lasso",n,p,lam0)
-sp1$sel.set
-plot(sp1$beta.hat,ylim=c(-0.5,1.5))
-points(s0,beta0[s0],pch=19,col="red")
-
+###########run SPARES
 sps<-SPARES(xmat,yvec,selection="lasso",nloop=500)
 summary(sps)
+#sm.beta: SPARES estimator
+#sd: standard deviation estimate
+#p: p-values of testing \beta^0_j = 0
+#sel.freq: selection frequency for the predictors
+#
+
 sps$int
-plot(sps$sm.beta,ylim=c(-0.5,1.5))
+sps$nzero
+plot(sps$sm.beta,ylim=c(-0.5,2.5))
 points(s0,beta0[s0],pch=19,col="red")
 plot(as.table(sps$sel.freq))
-(1:p)[sps$p<0.05/p]
+
+s0
 
 coefmat<-cbind(1:p,sps$sm.beta,sps$p)
 head(coefmat[order(sps$p),])
-s0
--- SPARES.R
+++ SPARES.R
@@ -18,37 +18,6 @@ library(glmnet);
 library(doParallel);
 library(foreach);
 #######
-PaRes<-function(x1,hs,m){
-  t(x1)%*%(diag(rep(1,m))-hs)%*%x1
-}
-
-SPARE<-function(xmat,yvec,selection="lasso",n,p,lam0){
-  
-  samp1<-sample(1:n,n/2,replace=T)
-  tab1<-table(samp1)
-  samp2<-(1:n)[-as.numeric(names(tab1))]
-  n2<-length(samp2)
-  
-  l2<-glmnet(xmat[samp2,],yvec[samp2])
-  lam2<-(1:length(l2$lambda))[l2$lambda<lam0][1]
-  coef2<-l2$beta[,lam2]
-  set<-(1:p)[coef2!=0]
-  
-  beta_hat<-rep(0,p)
-  xs<-xmat[samp1,set]
-  pxs<-solve(t(xs)%*%xs)
-  hs<-xs%*%pxs%*%t(xs)
-  beta_hat[set]<-pxs%*%t(xs)%*%yvec[samp1]
-  
-  x_s<-xmat[samp1,-set]
-  h_s<-apply(x_s,2,PaRes,hs=hs,m=n/2)
-  beta_hat[-set]<-(t(x_s)/h_s)%*%(diag(rep(1,n/2))-hs)%*%yvec[samp1]
-  yicount<-rep(0,n)
-  yicount[-samp2]<-tab1
-  
-  returnList<-list("beta.hat"=beta_hat,"sel.set"=set,"boot.ct"=yicount)
-  return(returnList)
-}
 
 extr<-function(fit1,k,pos){
   temp1<-fit1[[k]][[pos]]
@@ -64,6 +33,38 @@ sdu<-function(beta,Ycount,nloop,n){
 }
 
 SPARES<-function(xmat,yvec,selection="lasso",nloop=500){
+  PaRes<-function(x1,hs,m){
+    t(x1)%*%(diag(rep(1,m))-hs)%*%x1
+  }
+  
+  SPARE<-function(xmat,yvec,selection="lasso",n,p,lam0){
+    
+    samp1<-sample(1:n,n/2,replace=T)
+    tab1<-table(samp1)
+    samp2<-(1:n)[-as.numeric(names(tab1))]
+    n2<-length(samp2)
+    
+    l2<-glmnet(xmat[samp2,],yvec[samp2])
+    lam2<-(1:length(l2$lambda))[l2$lambda<lam0][1]
+    coef2<-l2$beta[,lam2]
+    set<-(1:p)[coef2!=0]
+    
+    beta_hat<-rep(0,p)
+    xs<-xmat[samp1,set]
+    pxs<-solve(t(xs)%*%xs)
+    hs<-xs%*%pxs%*%t(xs)
+    beta_hat[set]<-pxs%*%t(xs)%*%yvec[samp1]
+    
+    x_s<-xmat[samp1,-set]
+    h_s<-apply(x_s,2,PaRes,hs=hs,m=n/2)
+    beta_hat[-set]<-(t(x_s)/h_s)%*%(diag(rep(1,n/2))-hs)%*%yvec[samp1]
+    yicount<-rep(0,n)
+    yicount[-samp2]<-tab1
+    
+    returnList<-list("beta.hat"=beta_hat,"sel.set"=set,"boot.ct"=yicount)
+    return(returnList)
+  }
+  
   n<-dim(xmat)[1]
   p<-dim(xmat)[2]
   
@@ -86,12 +87,14 @@ SPARES<-function(xmat,yvec,selection="lasso",nloop=500){
   betam<-rowMeans(BETA)
   sds<-apply(BETA,1,sdu,Ycount=(Ycount),nloop=nloop,n=n)
   pvs<-2*(1-pnorm(abs(betam)/sds))
+  nzero<-(1:p)[pvs<0.05/p]
+  int<-y0-sum(betam[nzero]*colMeans(xmat[,nzero])/sdx[nzero])
   
   temptab<-table(unlist(SET))
   sel_freq<-rep(0,p)
   sel_freq[as.numeric(names(temptab))]<-temptab/nloop
   
   returnList<-list("sm.beta"=betam/sdx,"sd"=sds/sdx,"p"=pvs,"sel.freq"=sel_freq,
-                   "int"=y0-sum(betam*colMeans(xmat)/sdx))
+                   "int"=int,"nzero"=nzero)
   return(returnList)
 }
