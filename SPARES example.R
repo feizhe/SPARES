2e3f524f7ec3ff1e4a8b31520f4b239b00254487
feizhe <feizhe@users.noreply.github.com>
2017-03-14 11:54:44 -0400

    Add files via upload

--- SPARES example.R
+++ SPARES example.R
@@ -30,19 +30,20 @@ summary(lm0)
 ###########run SPARES
 sps<-SPARES(xmat,yvec,selection="lasso",nloop=500)
 summary(sps)
+
 #sm.beta: SPARES estimator
 #sd: standard deviation estimate
 #p: p-values of testing \beta^0_j = 0
 #sel.freq: selection frequency for the predictors
-#
+#int: intercept estimate
+#nzero: non-zero signals chosen by p-values and Bonferroni correction
 
 sps$int
 sps$nzero
-plot(sps$sm.beta,ylim=c(-0.5,2.5))
-points(s0,beta0[s0],pch=19,col="red")
-plot(as.table(sps$sel.freq))
+plot(sps$sm.beta,ylim=c(-0.5,2.5))      #SPARES estimator
+points(s0,beta0[s0],pch=19,col="red")   #truth
+points(s0,coef(lm0)[-1],col="blue")     #oracle estimates
 
-s0
-
-coefmat<-cbind(1:p,sps$sm.beta,sps$p)
+###########summary table
+coefmat<-cbind(1:p,sps$sm.beta,sps$sd,sps$p)
 head(coefmat[order(sps$p),])
